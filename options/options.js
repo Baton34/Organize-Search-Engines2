da248d744e71c689745b27e0b72d784736f00fc5
Baton34 <>
2021-10-20 16:05:29 +0300

    folder icon themed

--- options/options.js
+++ options/options.js
@@ -15,9 +15,24 @@
 		menuItem.setAttribute('draggable',true);
 		const icon=document.createElement('span');
 		icon.setAttribute('class','icon');
-		const image=document.createElement('img');
-		image.setAttribute('src',rootChild.icon);
-		icon.appendChild(image);
+		if (rootChild.type == 1) {
+			const image=document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+			image.setAttribute('viewBox','0 0 16 16');
+			image.setAttribute('width','20');
+			image.setAttribute('height','20');
+			const iconPath = document.createElementNS('http://www.w3.org/2000/svg','path');
+			iconPath.setAttribute(
+				'd',
+				'm13.995 14-10.99 0A2.007 2.007 0 0 1 1 11.995l0-8.99C1 1.899 1.899 1 3.005 1l2.958 0a2.01 2.01 0 0 1 1.47.641L8.693 3l5.302 0C15.101 3 16 3.899 16 5.005l0 6.99A2.007 2.007 0 0 1 13.995 14zM2.85 2.25l-.6.6 0 9.3.6.6 11.3 0 .6-.6 0-7.3-.6-.6-5.73 0-.458-.2-1.445-1.559a.758.758 0 0 0-.554-.241l-3.113 0z'
+			);
+			image.appendChild(iconPath);
+			icon.appendChild(image);
+		}
+		else {
+			const image=document.createElement('img');
+			image.setAttribute('src',rootChild.icon);
+			icon.appendChild(image);
+		}
 		menuItem.appendChild(icon);
 		const text=document.createElement('span');
 		text.setAttribute('class','text');
@@ -198,7 +213,7 @@
 		}
 		let node=new Node(engineName);
 		node.type=1;
-		node.icon=folderIcon;
+		node.icon='';
 		tree.addNode(node);
 		renderList();
 	});
